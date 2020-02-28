f1e5ec23c4a8004696bbadd20542a5786bd14577
Vanessa McHale <vamchale@gmail.com>
2020-02-28 16:02:52 -0600

    Bump smake

--- src/Package/C/PackageSet.hs
+++ src/Package/C/PackageSet.hs
@@ -24,7 +24,7 @@ import           Package.C.Type.Tree
 
 defaultPackageSetDhall :: Maybe String -> IO PackageSetDhall
 defaultPackageSetDhall (Just pkSet) = input auto (T.pack pkSet)
-defaultPackageSetDhall Nothing      = input auto "https://raw.githubusercontent.com/vmchale/cpkg/ea4e5bb39364b6ef44b4c4fea69163263d59dde0/pkgs/pkg-set.dhall sha256:66f21b9803dc5ca046868eb10ea08c70102fc00da12471b46414f2335b928a7b"
+defaultPackageSetDhall Nothing      = input auto "https://raw.githubusercontent.com/vmchale/cpkg/cd2ee76326423fddcb93eb230f88f5c3d2234c5e/pkgs/pkg-set.dhall sha256:7191b3254b6b7a59d139c1a89c61ef573077493e25b2ca4d2cea599d7b08103a"
 
 displayPackageSet :: Maybe String -> IO ()
 displayPackageSet = putDoc . pretty <=< defaultPackageSetDhall
